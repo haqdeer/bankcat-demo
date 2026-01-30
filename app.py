fc4a99436cad01a9d9ce0b94a7d7d7c0c1cde3c9
Hafiz Abdul Qadeer <qadeerhaqqani@gmail.com>
2026-01-30 17:35:25 +0500

    Update app.py

--- app.py
+++ app.py
@@ -365,7 +365,7 @@ def _select_active_client(clients):
     return client_id
 
 def _select_bank(banks_active):
-    bank_options = [f"{b['id']} | {b['bank_name']} ({b['account_type()})" for b in banks_active]
+    bank_options = [f"{b['id']} | {b['bank_name']} ({b['account_type'})" for b in banks_active]
     selected_index = 0
     if st.session_state.bank_id:
         for i, opt in enumerate(bank_options):
